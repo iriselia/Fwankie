e7ca9350829710989fd30c41ae8301820224ce47
piaoasd123 <piaoasd123@163.com>
2014-06-19 02:07:14 +0800

    Added MYGUI OIS and FREETYPE.

---
+++ 3rdParty/MyGUI/updateListFiles.bat
@@ -0,0 +1,2 @@
+chdir C:/Users/piaoa_000/Documents/GitHub/Fwankie/3rdParty/MyGUI/Scripts
+C:/Users/piaoa_000/Documents/GitHub/Fwankie/3rdParty/MyGUI/Scripts/parseXML.exe C:/Users/piaoa_000/Documents/GitHub/Fwankie/3rdParty/MyGUI
